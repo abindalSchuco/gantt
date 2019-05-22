258f84f2445a236371c6647d94247723e13225f5
Sudeep Reddy <sudeep04@users.noreply.github.com>
2019-05-22 12:37:27 +0900

    Updated URL

--- codebase/ext/dhtmlxgantt_export_to_pdf.js
+++ codebase/ext/dhtmlxgantt_export_to_pdf.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var apiUrl = "http://52.207.214.86/gantt/";
+    var apiUrl = "https://schexport.dev.plantobuild.de/gantt/";
 
     var templates = [
         "leftside_text",
@@ -649,7 +649,7 @@
 })();
 
 (function () {
-    var apiUrl = "http://52.207.214.86/gantt/";
+    var apiUrl = "https://schexport.dev.plantobuild.de/gantt/";
 
     function set_level(data) {
         for (var i = 0; i < data.length; i++) {
