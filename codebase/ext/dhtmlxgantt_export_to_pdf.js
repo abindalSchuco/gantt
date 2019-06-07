31652a6b64a7f00b1d88fd21edf958ab5715db63
abindalSchuco <abindal@schuco-usa.com>
2019-06-07 13:57:27 -0400

    Update dhtmlxgantt_export_to_pdf.js

--- codebase/ext/dhtmlxgantt_export_to_pdf.js
+++ codebase/ext/dhtmlxgantt_export_to_pdf.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var apiUrl = "https://schexport.dev.plantobuild.de/gantt/";
+    var apiUrl = "https://schexport.cmacspro.com/gantt/";
 
     var templates = [
         "leftside_text",
@@ -649,7 +649,7 @@
 })();
 
 (function () {
-    var apiUrl = "https://schexport.dev.plantobuild.de/gantt/";
+    var apiUrl = "https://schexport.cmacspro.com/gantt/";
 
     function set_level(data) {
         for (var i = 0; i < data.length; i++) {
